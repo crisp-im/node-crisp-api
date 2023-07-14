eec25744834038ee79ba528320adffdb1aec10dc
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-14 12:50:52 +0200

    Add all now-public Website Helpdesk routes

---
+++ types/resources/WebsiteHelpdesk.d.ts
@@ -0,0 +1,15 @@
+export = WebsiteHelpdesk;
+/**
+ * Crisp WebsiteHelpdesk Resource
+ * @class
+ * @classdesc This is the Crisp Website Helpdesk Resource
+ */
+declare function WebsiteHelpdesk(service: any, crisp: any): void;
+declare class WebsiteHelpdesk {
+    /**
+     * Crisp WebsiteHelpdesk Resource
+     * @class
+     * @classdesc This is the Crisp Website Helpdesk Resource
+     */
+    constructor(service: any, crisp: any);
+}
